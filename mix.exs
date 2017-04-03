58472ca52b34fd7b4a515ed08b57dabbdb67a8de
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2017-04-03 08:44:09 +0200

    mix.exs for easy Elixir package.

---
+++ mix.exs
@@ -0,0 +1,31 @@
+defmodule Erlcron.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :erlcron,
+     version: "0.4.0",
+     build_path: "../../_build",
+     config_path: "../../config/config.exs",
+     deps_path: "../../deps",
+     lockfile: "../../mix.lock",
+     elixir: "~> 1.3",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps()]
+  end
+
+  def application do
+    [extra_applications: [], mod: {:ecrn_app, []}]
+  end
+
+  # Dependencies can be Hex packages:
+  #
+  #   {:my_dep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+  #
+  # Type "mix help deps" for more examples and options
+  defp deps, do: []
+end
